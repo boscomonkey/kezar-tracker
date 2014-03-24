48be58f96861a6315467771601a4a9bc49c1ca97
Bosco So <bosco@sfruby.info>
2014-03-24 10:48:01 -0700

    spiking timer and geolocation code

--- mobile/platforms/ios/www/js/index.js
+++ mobile/platforms/ios/www/js/index.js
@@ -34,6 +34,8 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        app.startTimer('timestamp');
+        app.startGeo();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -45,5 +47,68 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    geoId: null,
+    // start monitoring geolocation
+    startGeo: function() {
+        var latitude = document.getElementById('latitude');
+        var longitude = document.getElementById('longitude');
+        var altitude = document.getElementById('altitude');
+        var speed = document.getElementById('speed');
+        var onSuccess = function(suc) {
+            latitude.innerHTML = String(suc.coords.latitude);
+            longitude.innerHTML = String(suc.coords.longitude);
+            altitude.innerHTML = String(suc.coords.altitude);
+            speed.innerHTML = String(suc.coords.speed);
+
+            //// console.log(suc);
+        };
+        var onError = function(err) {
+            var error = document.getElementById('error');
+            error.innerHTML = 'error: ' + JSON.stringify(err);
+
+            //// console.log(err);
+        };
+        app.geoId = navigator.geolocation.watchPosition(onSuccess, onError);
+    },
+    timerId: null,
+    timerInterval: 50,
+    // start a timer to update ID periodically
+    startTimer: function(elementId) {
+        var element = document.getElementById(elementId);
+        var interval = 1000;
+        app.timerId = setInterval(
+            function() {
+                var dt = new Date();
+                var hour = app.util.padLeadingZero(dt.getHours());
+                var min = app.util.padLeadingZero(dt.getMinutes());
+                var sec = app.util.padLeadingZero(dt.getSeconds());
+                var ms = app.util.padLeadingZero(app.util.twoDigitsMilliseconds(dt));
+                var str = hour + ':' + min + ':' + sec + '.' + ms;
+
+                element.innerHTML = str;
+            },
+            app.timerInterval
+        );
+    },
+    util: {
+        padLeadingZero: function(num) {
+            var str = String(num);
+
+            switch (str.length) {
+            case 0:
+                return '00';
+            case 1:
+                return '0' + str;
+            default:
+                return str;
+            }
+        },
+        twoDigitsMilliseconds: function(dt) {
+            var ms = dt.getMilliseconds();
+            var digits2 = Math.round(ms / 10);
+
+            return 100 == digits2 ? 0 : digits2;
+        },
     }
 };
