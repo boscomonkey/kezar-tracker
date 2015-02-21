29cfd3acae399b6a05d417e3d1377391136d5aac
Bosco So <boscoso@halcyon.local>
2015-02-21 13:51:55 -0500

    renaming app to 'KezarTracker'; upgrading cordova; added android 	cordova create mobile com.boscomonkey.kezar 'KezarTracker' 	cordova platform add android 	cordova platform add ios 	cordova plugin add org.apache.cordova.console 	cordova plugin add org.apache.cordova.geolocation 	cordova build 	cordova emulate android 	cordova emulate ios

---
+++ mobile/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/boscoso/Documents/workspace/kezar-tracker/mobile/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/boscoso/Documents/workspace/kezar-tracker/mobile/platforms/android/AndroidManifest.xml \
---
+++ mobile/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/boscoso/Documents/workspace/kezar-tracker/mobile/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/Users/boscoso/Documents/workspace/kezar-tracker/mobile/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/boscoso/Documents/workspace/kezar-tracker/mobile/platforms/android/ant-build/classes.dex \
+/Users/boscoso/Documents/workspace/kezar-tracker/mobile/platforms/android/CordovaLib/ant-build/classes.jar \
