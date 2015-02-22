34301ad503619b3a56845e02e9a2eba833de108f
Bosco So <boscoso@pif-bosco-mac.local>
2015-02-22 01:33:20 -0500

    geo-watching in native app entry point

--- mobile/www/js/native-or-web.js
+++ mobile/www/js/native-or-web.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
     var logger = $('.status');
-    var errors = $('.errors');
 
-    function err() {
+    var logError = function() {
         var container = document.createElement('li');
         var line;
 
@@ -13,18 +12,70 @@ $(document).ready(function() {
             $(container).append($(line));
         }
 
-        errors.prepend($(container))
+        $('.errors').prepend($(container))
+    };
+
+    var isNativeApp = function( script, textStatus ) {
+        logger.text('Native App');
+        document.addEventListener('deviceready', runApp, false);
+    };
+
+    var isWebApp = function( jqxhr, settings, exception ) {
+        logger.text('Web App');
+        runApp();
+    };
+
+    function runApp() {
+        var alertStart = function() {
+            var elt = document.createElement('h2');
+
+            elt.textContent = 'App has started!';
+            $('body').append($(elt));
+        };
+        var alertPing = function() {
+            $('body').fadeOut().fadeIn();
+        };
+        var blinkBackground = function() {
+            var body = $('body');
+            var color = body.css('background-color');
+
+            body.css('background-color', 'yellow');
+            setTimeout(
+                function() { body.css('background-color', color) },
+                50
+            );
+        };
+        var fields = [
+            'accuracy',
+            'altitude',
+            'altitudeAccuracy',
+            'heading',
+            'latitude',
+            'longitude',
+            'speed',
+        ];
+        var geoSuccess = function(position) {
+            alertPing();
+            for (var ii in fields) {
+                var field = fields[ii];
+                var elt = document.getElementById(field);
+                var data = position.coords[field];
+
+                elt.textContent = data;
+            };
+        };
+        var geoFailure = function(positionError) {
+            logError(positionError);
+        };
+
+        alertStart();
+        navigator.geolocation.watchPosition(
+            geoSuccess, geoFailure, {enableHighAccuracy: true}
+        );
     }
 
-    $.getScript('cordova.js')
-        .done(function( script, textStatus ) {
-            logger.text(textStatus);
-        })
-        .fail(function( jqxhr, settings, exception ) {
-            logger.text('NOT FOUND');
-
-            err('jqxhr:', JSON.stringify(jqxhr));
-            err('settings:', settings);
-            err('exception:', exception);
-        });
+    /*
+     * use presence of cordova.js to determine whether native or web
+     */
+    $.getScript('cordova.js').done(isNativeApp).fail(isWebApp);
 });
